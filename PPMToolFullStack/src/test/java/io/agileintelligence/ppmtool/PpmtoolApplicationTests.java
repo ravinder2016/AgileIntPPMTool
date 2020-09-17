33ad569a8b46498d9430d03652219069103558ff
Karumula <Raveender.Karumula@parexel.com>
2020-09-17 19:35:10 +0530

    first commit

---
+++ PPMToolFullStack/src/test/java/io/agileintelligence/ppmtool/PpmtoolApplicationTests.java
@@ -0,0 +1,12 @@
+package io.agileintelligence.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolApplicationTests {
+  @Test
+  void contextLoads() {
+  }
+
+}
