4da06dba1a0216ad3f7fdeaadb82da6f899e0b45
Karumula <Raveender.Karumula@parexel.com>
2020-09-21 16:11:13 +0530

    Redux store setup is done

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -5,16 +5,20 @@ import Header from './Layout/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import AddProject from './components/Project/AddProject';
+import { Provider } from 'react-redux';
+import store from './store';
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Header />
-          <Dashboard />
-          <Route exact path="/addProject" component={AddProject} />
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Header />
+            <Dashboard />
+            <Route exact path="/addProject" component={AddProject} />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
